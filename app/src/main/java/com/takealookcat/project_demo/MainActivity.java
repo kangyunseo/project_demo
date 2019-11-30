d8b3d12a9513d2c68a367be19f8cb7098650e801
kangyunseo <kangyunseo@sogang.ac.kr>
2019-12-01 03:20:06 +0900

    init 3 & gitignore

--- app/src/main/java/com/takealookcat/project_demo/MainActivity.java
+++ app/src/main/java/com/takealookcat/project_demo/MainActivity.java
@@ -6,9 +6,7 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
