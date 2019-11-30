902fd541308c978a069bd74ca91b366a0b4c70a9
kangyunseo <kangyunseo@sogang.ac.kr>
2019-12-01 03:43:54 +0900

    init 5 in desktop

--- app/src/main/java/com/takealookcat/project_demo/catitem.java
+++ app/src/main/java/com/takealookcat/project_demo/catitem.java
@@ -2,7 +2,7 @@ package com.takealookcat.project_demo;
 
 //git push from notebook
 //git push from desktop
-//test brc
+//test brc 2
 import android.graphics.drawable.Drawable;
 
 public class catitem {
