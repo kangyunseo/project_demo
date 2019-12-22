1ad482fcfad5c7106050d920e863b57200d9b990
kangyunseo <kangyunseo@sogang.ac.kr>
2019-12-22 15:28:35 +0900

    4번 메뉴 스타일 수정

--- app/src/main/java/com/takealookcat/project_demo/menu_4.java
+++ app/src/main/java/com/takealookcat/project_demo/menu_4.java
@@ -421,7 +421,6 @@ public class menu_4 extends Fragment implements  View.OnClickListener {
         public void onDataChange(DataSnapshot dataSnapshot) {
             //여기에 조건(버튼)
             //리스너 방식임
-
                 m_mapPoint2.clear();
                 // 위에 선언한 저장소인 datas를 초기화하고
                 // donation 레퍼런스의 스냅샷을 가져와서 레퍼런스의 자식노드를 반복문을 통해 하나씩 꺼내서 처리.
